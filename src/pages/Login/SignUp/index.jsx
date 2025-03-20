d076645be8263a69556269a037035e6bc8cb5c35
pushpalatha1993 <ravurupushpalatha@gmail.com>
2025-03-20 21:27:14 +0530

    login sinup ui1

--- src/pages/Login/SignUp/index.jsx
+++ src/pages/Login/SignUp/index.jsx
@@ -1,63 +1,91 @@
-import React, { useState } from 'react'
+import React, {Component} from "react";
 import "./style.css"
 import { Link } from 'react-router-dom';
-export default function SignUp() {
+export default class  SignUp extends Component {
 
 
-    const [email, setEmail] = useState();
-    const [password, stePassword] = useState();
-    const [username, setUsername] = useState();
+    // const [email, setEmail] = useState();
+    // const [password, stePassword] = useState();
+    // const [username, setUsername] = useState();
 
-    const handleSignUp = () => {
-        alert("Sign Up successful")
+    constructor(props){
+        super(props);
+        this.state = {
+            username:  " ",
+            email: " ",
+            password: " "
+
+        }
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
 
+    handleSubmit = (event) => {
+        event.preventDefault()
+       const response = fetch(" https://instaapp-np7g.onrender.com/api/auth/register/email ",{           
+            method: "POST",
+           headers: {
+               'Content-Type': 'application/json',
+           },
+           body: JSON.stringify({name:this.state.name,email:this.state.email,password:this.state.password}),
+           
+       })
+       console.dir(response)
+    }
 
-    return (
-        <div className="row g-0 vh-100 justify-content-center align-items-center SignUp-container">
+ render () {
 
-            <div className="col-10 row g-0 align-items-center border rounded-2 bg-white">
-                <div className="d-none d-md-block col-6">
-                    <img src="https://img.freepik.com/free-vector/lost-island-ocean-with-alone-castaway-person_107791-621.jpg?t=st=1742302383~exp=1742305983~hmac=1099f3747d8fa6374232a7150518acac4042b6b43c3bc1975f28f7042691edf4&w=826" alt="" className='img-fluid' />
-                </div>
-                <form className="col-12 col-md-6 py-4 px-3">
-                    <h4 className="SignUp-title text-center py-2 mb-4">SignUp</h4>
-                    <div className="form-floating mb-3">
-                        <input type="text" className="form-control" id="username" placeholder="codediggy" onChange={(e) => { setEmail(e.target.value) }} />
-                        <label htmlFor="username">Username</label>
-                    </div>
+     return (
+    <div className="row g-0 vh-100 justify-content-center align-items-center SignUp-container">
 
-                    <div className="form-floating mb-3">
-                        <input type="email" className="form-control" id="email" placeholder='name@example.com' onChange={(e) => { setEmail(e.target.value) }} />
-                        <label htmlFor="email">Email</label>
-                    </div>
-
-                    <div className="form-floating mb-3">
-                        <input type="password" className="form-control" placeholder='password' id="password" onChange={(e) => { stePassword(e.target.value) }} />
-                        <label htmlFor="password">password</label>
-
-                    </div>
-                    <div className="text-center">
-                        <button className="signup-btn py-3 rounded-3 onClick={()=>{handleLogin()}}">
-                            SignUp
-                        </button>
-                    </div>
-
-                    <div className="text-center mt-4">  Already Registered ? <Link to="/login">Login</Link>
-
-
-                    </div>
-
-
-
-
-
-                </form>
-
-
-
+        <div className="col-10 row g-0 align-items-center border rounded-2 bg-white">
+            <div className="d-none d-md-block col-6">
+                <img src="https://img.freepik.com/free-vector/lost-island-ocean-with-alone-castaway-person_107791-621.jpg?t=st=1742302383~exp=1742305983~hmac=1099f3747d8fa6374232a7150518acac4042b6b43c3bc1975f28f7042691edf4&w=826" alt="" className='img-fluid' />
             </div>
+            <form className="col-12 col-md-6 py-4 px-3">
+                <h4 className="SignUp-title text-center py-2 mb-4">SignUp</h4>
+                <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="username" placeholder="codediggy" onChange={ this.handleChange} />
+                    <label htmlFor="username">Username</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="email" placeholder='name@example.com' onChange={this.handleChange} />
+                    <label htmlFor="email">Email</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                    <input type="password" className="form-control" placeholder='password' id="password" onChange={this.handleChange} />
+                    <label htmlFor="password">password</label>
+
+                </div>
+                <div className="text-center">
+                    <button className="signup-btn py-3 rounded-3" onClick={this.handleSubmit}>
+                        SignUp
+                    </button>
+                </div>
+
+                <div className="text-center mt-4">  Already Registered ? <Link to="/login">Login</Link>
+
+
+                </div>
+
+
+
+
+
+            </form>
+
+
+
         </div>
-    )
+    </div>
+)}
+   
+
 }
+
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,65 +1,81 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import "./style.css"
 import { Link } from 'react-router-dom';
-export default function Login() {
+export default class Login extends Component {
 
 
-    const [email, setEmail] = useState();
-    const [password, stePassword] = useState();
+    // const [email, setEmail] = useState();
+    // const [password, stePassword] = useState();
+    constructor(props){
+        super(props);
+        this.state = {
+            email:" ",
+            password:" "
 
-    const handleLogin = () => {
-        alert("Login successful")
+        }
     }
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+ }
+ handleSubmit = (event) => {
+    event.preventDefault()
+ }
 
+    // const handleLogin = () => {
+        // alert("Login successful")
+    // }
 
+render () { return (
+    <div className="row g-0 vh-100 justify-content-center align-items-center login-container">
 
-    return (
-        <div className="row g-0 vh-100 justify-content-center align-items-center login-container">
-
-            <div className="col-10 row g-0 align-items-center border rounded-2 bg-white">
-                <div className="d-none d-md-block col-6">
-                    <img src="https://img.freepik.com/premium-vector/small-island_645480-1472.jpg?w=826" alt="" className='img-fluid' />
-                </div>
-                <form className="col-12 col-md-6 py-4 px-3">
-                    <h4 className="login-title text-center py-2 mb-4">Login</h4>
-                    <div className="form-floating mb-3">
-                        <input type="email" className="form-control" id="email" placeholder='name@example.com' onChange={(e) => { setEmail(e.target.value) }} />
-                        <label htmlFor="email">Email</label>
-                    </div>
-
-
-                    <div className="form-floating mb-3">
-                        <input type="password" className="form-control" placeholder='password' id="password" onChange={(e) => { stePassword(e.target.value) }} />
-                        <label htmlFor="password">password</label>
-
-                    </div>
-                    <div className="text-center">
-                        <button className="login-btn py-3 rounded-3 onClic={()=>{handleLogin()}}">
-                            Login
-                        </button>
-                    </div>
-
-                    <div className="text-center mt-4"> Not registered ?<Link to="/signup">Sign Up</Link>
-
-
-                    </div>
-
-
-
-
-
-                </form>
-
-
-
-
-
-
-
-
-
-
+        <div className="col-10 row g-0 align-items-center border rounded-2 bg-white">
+            <div className="d-none d-md-block col-6">
+                <img src="https://img.freepik.com/premium-vector/small-island_645480-1472.jpg?w=826" alt="" className='img-fluid' />
             </div>
+            <form className="col-12 col-md-6 py-4 px-3">
+                <h4 className="login-title text-center py-2 mb-4">Login</h4>
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="email" placeholder='name@example.com' onChange={this.handleChange } />
+                    <label htmlFor="email">Email</label>
+                </div>
+
+
+                <div className="form-floating mb-3">
+                    <input type="password" className="form-control" placeholder='password' id="password" onChange={this.handleChange} />
+                    <label htmlFor="password">password</label>
+
+                </div>
+                <div className="text-center">
+                    <button className="login-btn py-3 rounded-3" onClick={this.handleSubmit}>
+                        Login
+                    </button>
+                </div>
+
+                <div className="text-center mt-4"> Not registered ?<Link to="/signup">Sign Up</Link>
+
+
+                </div>
+
+
+
+
+
+            </form>
+
+
+
+
+
+
+
+
+
+
         </div>
-    )
+    </div>
+)}
+
+   
 }
